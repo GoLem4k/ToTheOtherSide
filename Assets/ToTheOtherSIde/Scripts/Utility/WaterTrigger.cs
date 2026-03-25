469368f0b0af93021112389d4e388abbb2b1bd2e
Unio <147451837+GoLem4k@users.noreply.github.com>
2026-03-25 18:51:19 +0300

    item destroy by water

--- Assets/ToTheOtherSIde/Scripts/Utility/WaterTrigger.cs
+++ Assets/ToTheOtherSIde/Scripts/Utility/WaterTrigger.cs
@@ -11,5 +11,10 @@ public class WaterTrigger : MonoBehaviour
             SceneManager.LoadScene(currentScene.buildIndex);
             Debug.Log($"Перезагружена текущая сцена: {currentScene.name}");
         }
+
+        if (other.CompareTag("Item"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
